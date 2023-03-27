fef187901a24af5a3cb7978d2a41779e998d8576
CrinaCosmina <angheluscozmina@yahoo.com>
2023-03-27 16:57:39 +0300

    Add BookStore controller and first migration.

---
+++ OnlineBookstore/Controllers/BookStoreController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.DTO;
+using OnlineBookstore.Services;
+
+namespace OnlineBookstore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookStoreController : Controller
+    {
+        private readonly IBookStoreService _bookStoreService;
+
+        public BookStoreController(IBookStoreService bookStoreService)
+        {
+            _bookStoreService = bookStoreService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var bookStores = await _bookStoreService.GetAll();
+
+            if (bookStores == null)
+            {
+                return BadRequest("No BookStores found.");
+            }
+
+            return Ok(bookStores);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest("The id can't be null!");
+            }
+
+            var bookStore = await _bookStoreService.Get(id);
+
+            if (bookStore == null)
+            {
+                return NotFound("The BookStore doesn't exist!");
+            }
+
+            return Ok(bookStore);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] BookStoreDTO bookStore)
+        {
+            if (bookStore == null)
+            {
+                return BadRequest("The BookStore can't be null!");
+            }
+
+            if (await _bookStoreService.Exists(bookStore.Name, -1))
+            {
+                return BadRequest("There is already a BookStore with that name.");
+            }
+
+            if (await _bookStoreService.Add(bookStore))
+            {
+                return Ok(bookStore);
+            }
+            return BadRequest("The BookStore was invalid!");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(int id, [FromBody] BookStoreDTO bookStore)
+        {
+            if (!await _bookStoreService.Exists(id))
+            {
+                return BadRequest("There is no BookStore with that id.");
+            }
+            if (await _bookStoreService.Exists(bookStore.Name, id))
+            {
+                return BadRequest("There is already a BookStore with that name.");
+            }
+
+            if (await _bookStoreService.Update(bookStore, id))
+            {
+                return Ok(bookStore);
+            }
+            return BadRequest("Invalid BookStore!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await _bookStoreService.GetAll() == null)
+            {
+                return BadRequest("No BookStore found.");
+            }
+
+            if (await _bookStoreService.Get(id) == null)
+            {
+                return NotFound("The BookStore doesn't exist!");
+            }
+
+            if (await _bookStoreService.Delete(id))
+            {
+                return Ok("The BookStore was successfully deleted!");
+            }
+            return BadRequest("Invalid id!");
+        }
+    }
+}
---
+++ OnlineBookstore/Migrations/20230327135618_MyFirstMigration.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineBookstore.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyFirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookStore",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStore", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BooksStock",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    bookId = table.Column<int>(type: "int", nullable: false),
+                    bookStoreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BooksStock", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BooksStock_BookStore",
+                        column: x => x.bookId,
+                        principalTable: "Book",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BooksStock_BookStore_bookStoreId",
+                        column: x => x.bookStoreId,
+                        principalTable: "BookStore",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BooksStock_bookId",
+                table: "BooksStock",
+                column: "bookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BooksStock_bookStoreId",
+                table: "BooksStock",
+                column: "bookStoreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BooksStock");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "BookStore");
+        }
+    }
+}
